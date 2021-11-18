051e968074ae5096f9661a7d435aa02ed6ac14e8
Justin Kim <justin1004@gmail.com>
2021-11-18 23:02:43 +0900

    first session done

--- src/App_v10.js
+++ src/App_v10.js
@@ -6,50 +6,39 @@ import './App.css';
 class App extends Component {
     state = {
         counters: [
-            { id: 1, value: 4 },
-            { id: 2, value: 0 },
-            { id: 3, value: 0 },
-            { id: 4, value: 0 },
+          { id: 1, value: 4 },
+          { id: 2, value: 0 },
+          { id: 3, value: 0 },
+          { id: 4, value: 0 },
         ],
-    };
-
-      // Mounting Phase
-    constructor() {
-        super();
-        console.log('App - Constructor');
-        // this.state = this.props.something;
-    }
-
-    componentDidMount() {
-        // Ajax call
-        console.log('App - Mounted');
-    }
+      };
     
-    handleIncrement = (counter) => {
+      handleIncrement = (counter) => {
         const counters = [...this.state.counters];
         const index = counters.indexOf(counter);
         counters[index] = { ...counter };
         counters[index].value++;
         console.log(this.state.counters[index]);
         this.setState({ counters });
-    };
+      };
     
-    handleReset = () => {
+      handleReset = () => {
         const counters = this.state.counters.map((c) => {
           c.value = 0;
           return c;
         });
         this.setState({ counters });
-    };
+      };
     
-    handleDelete = (counterId) => {
+      handleDelete = (counterId) => {
         // console.log("Event Handler Called", counterId);
         const counters = this.state.counters.filter((c) => c.id !== counterId);
         this.setState({ counters });
-    };
+      };
+    
+    
 
     render() {
-        console.log("App - Rendered");
         return (
             <React.Fragment>
                 <NavBar totalCounters={this.state.counters.filter(c => c.value > 0).length} />
---
+++ src/components/counters_v7.jsx
@@ -0,0 +1,38 @@
+import React, { Component } from "react";
+import Counter from "./counter";
+
+class Counters extends React.Component {
+  state = {
+    counters: [
+      { id: 1, value: 4 },
+      { id: 2, value: 0 },
+      { id: 3, value: 0 },
+      { id: 4, value: 0 },
+    ],
+  };
+
+  handleDelete = (counterId) => {
+    // console.log("Event Handler Called", counterId);
+    const counters = this.state.counters.filter((c) => c.id !== counterId);
+    this.setState({ counters });
+  };
+
+  render() {
+    return (
+      <div>
+        {this.state.counters.map((counter) => (
+          <Counter
+            key={counter.id}
+            onDelete={this.handleDelete}
+            value={counter.value}
+            id={counter.id}
+          >
+            <h4>Counter #{counter.id}</h4>
+          </Counter>
+        ))}
+      </div>
+    );
+  }
+}
+
+export default Counters;
